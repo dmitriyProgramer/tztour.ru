553506fdb8b369efe683aa90848f2d4ca278deb6
dmitriyProgramer <46451948+dmitriyProgramer@users.noreply.github.com>
2020-11-30 19:14:07 +0500

    Update save.js

--- save.js
+++ save.js
@@ -1,7 +1,7 @@
 $(document).ready(function(e) {
 	if( /test=true/.test( location.search ) ){
 		$('head').append('<link rel="stylesheet" type="text/css" href="wp-content/themes/nitro/assets/css/new_slider.css"/>');
-		$('head').append('<script type="text/javascript" src="wp-content/themes/nitro/assets/js/new_slider.js"></script>');
+		//$('head').append('<script type="text/javascript" src="wp-content/themes/nitro/assets/js/new_slider.js"></script>');
 		$('head').append('<link rel="stylesheet" type="text/css" href="wp-content/themes/nitro/assets/css/call_style.css"/>');
 		$('head').append('<link rel="stylesheet" type="text/css" href="wp-content/themes/nitro/assets/css/call_style_2.css"/>');
 		$('head').append('<script type="text/javascript" src="wp-content/themes/nitro/assets/js/key_phone.js"></script>');
