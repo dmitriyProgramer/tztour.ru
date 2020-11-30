2140d6aa0655e994f2c4663a1aa465ff79770978
dmitriyProgramer <46451948+dmitriyProgramer@users.noreply.github.com>
2020-11-30 15:35:04 +0500

    Update save.js

--- save.js
+++ save.js
@@ -2,6 +2,9 @@ $(document).ready(function(e) {
 	if( /test=true/.test( location.search ) ){
 		$('head').append('<link rel="stylesheet" type="text/css" href="wp-content/themes/nitro/assets/css/new_slider.css"/>');
 		$('head').append('<script type="text/javascript" src="wp-content/themes/nitro/assets/js/new_slider.js"></script>');
+		$('head').append('<link rel="stylesheet" type="text/css" href="wp-content/themes/nitro/assets/css/call_style.css"/>');
+		$('head').append('<link rel="stylesheet" type="text/css" href="wp-content/themes/nitro/assets/css/call_style_2.css"/>');
+		$('head').append('<script type="text/javascript" src="wp-content/themes/nitro/assets/js/key_phone.js"></script>');
 		$('head').append('<script type="text/javascript" src="//tourvisor.ru/module/init.js"></script>');
 	}
 });
